17c0af5f7a6dd653f52600e43cf434db49c0152d
David Coulombe <David.Coulombe7@gmail.com>
2017-12-12 22:36:59 -0500

    second

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,16 +9,27 @@
 	var losses = 0;
 	//variable for guesses thus far
 	var guesses = [];
+	//names computerGuess and uses random math to select one index of letters array
+	var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+		console.log(computerGuess);
 
 	//create function for onKeyUp
 	document.onkeyup = function() {
 		//names userGuess and sets to a string and sets to lower case
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 		console.log(userGuess);
-		//names computerGuess and uses random math to select one index of letters array
-		var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-		console.log(computerGuess);
 		
+		if(userGuess === computerGuess) {
+			alert('You WIN!');
+		}
+
+		if(userGuess!== computerGuess) {
+			chances--;
+		}
+
+		if(chances < 1) {
+			alert('You Lose');
+		}
 	}
 
 
